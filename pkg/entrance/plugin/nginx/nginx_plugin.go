9f8c9cb1b0f16d1742424ff33b767f13fd371c8e
崔斌 <cuiminmm@gmail.com>
2017-11-07 14:27:00 +0800

    [FIX]Fix can not add user-defined domain bug

--- pkg/entrance/plugin/nginx/nginx_plugin.go
+++ pkg/entrance/plugin/nginx/nginx_plugin.go
@@ -1,27 +1,24 @@
-
 // RAINBOND, Application Management Platform
 // Copyright (C) 2014-2017 Goodrain Co., Ltd.
- 
+
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version. For any non-GPL usage of Rainbond,
 // one or multiple Commercial Licenses authorized by Goodrain Co., Ltd.
 // must be obtained first.
- 
+
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
- 
+
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 package nginx
 
 import (
-	"github.com/goodrain/rainbond/pkg/entrance/core/object"
-	"github.com/goodrain/rainbond/pkg/entrance/plugin"
 	"bytes"
 	"errors"
 	"fmt"
@@ -29,6 +26,9 @@ import (
 	"net/http"
 	"regexp"
 	"strings"
+
+	"github.com/goodrain/rainbond/pkg/entrance/core/object"
+	"github.com/goodrain/rainbond/pkg/entrance/plugin"
 
 	"time"
 
@@ -439,7 +439,7 @@ func (n *nginxAPI) addDomain(ads *AddDomainS) bool {
 		Method:   MethodPOST,
 	}
 	n.pHTTP(pha)
-	if bytes.HasPrefix([]byte(ads.Domain), []byte(fmt.Sprintf("%s.%s", p.Port, p.Servicename))) {
+	if !bytes.HasPrefix([]byte(ads.Domain), []byte(fmt.Sprintf("%s.%s", p.Port, p.Servicename))) {
 		n.pHTTPDomain(ads.Domain, pha)
 	}
 	return true
