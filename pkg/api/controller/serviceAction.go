592560b98b7ae35de3ddf5acee15aa4eaf62ed32
bay1ts <bay1ts@163.com>
2017-11-24 13:50:10 +0800

    [REV] optimize event api structure

--- pkg/api/controller/serviceAction.go
+++ pkg/api/controller/serviceAction.go
@@ -38,8 +38,6 @@ import (
 	validator "github.com/thedevsaddam/govalidator"
 
 	"encoding/json"
-	"github.com/bitly/go-simplejson"
-	"io/ioutil"
 )
 
 //TIMELAYOUT timelayout
@@ -650,49 +648,7 @@ func (t *TenantStruct) CheckCode(w http.ResponseWriter, r *http.Request) {
 	httputil.ReturnSuccess(r, w, nil)
 }
 
-//Events GetLogs
-func (e *TenantStruct) Events(w http.ResponseWriter, r *http.Request) {
-	// swagger:operation GET  /v2/tenants/{tenant_name}/event v2 get events
-	//
-	// 获取指定event_ids详细信息  **完成
-	//
-	// get events
-	//
-	// ---
-	// produces:
-	// - application/json
-	// - application/xml
-	//
-	// responses:
-	//   default:
-	//     schema:
-	//       "$ref": "#/responses/commandResponse"
-	//     description: 统一返回格式
-	b,_:=ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	j,err:=simplejson.NewJson(b)
-	if err != nil {
-		logrus.Errorf("error decode json,details %s",err.Error())
-		httputil.ReturnError(r,w,400,"bad request")
-		return
-	}
-	eventIDS,err:=j.Get("event_ids").StringArray()
-	if err != nil {
-		logrus.Errorf("error get event_id in json,details %s",err.Error())
-		httputil.ReturnError(r,w,400,"bad request")
-		return
-	}
-	result:=[]*dbmodel.ServiceEvent{}
-	for _,v:=range eventIDS{
-		serviceEvent,err:=db.GetManager().ServiceEventDao().GetEventByEventID(v)
-		if err != nil {
-			logrus.Warnf("can't find event by given id %s ,details %s",v,err.Error())
-			continue
-		}
-		result=append(result,serviceEvent)
-	}
-	httputil.ReturnSuccess(r, w, result)
-}
+
 
 
 //ShareCloud 云市分享
