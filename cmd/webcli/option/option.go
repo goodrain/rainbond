39b9c8c2d2a5cb941d8eb07953f04b3399ffd072
ysicing <root@ysicing.net>
2017-11-29 14:04:30 +0800

    [FIX] Modify webcli default port 7171

--- cmd/webcli/option/option.go
+++ cmd/webcli/option/option.go
@@ -52,7 +52,7 @@ func (a *WebCliServer) AddFlags(fs *pflag.FlagSet) {
 	fs.StringSliceVar(&a.EtcdEndPoints, "etcd-endpoints", []string{"http://127.0.0.1:2379"}, "etcd v3 cluster endpoints.")
 	fs.StringVar(&a.Address, "address", "0.0.0.0", "server listen address")
 	fs.StringVar(&a.HostIP, "hostIP", "127.0.0.1", "webcli request ip address for regist lb")
-	fs.StringVar(&a.Port, "port", "7070", "server listen port")
+	fs.StringVar(&a.Port, "port", "7171", "server listen port")
 	fs.StringVar(&a.PrometheusMetricPath, "metric", "/metrics", "prometheus metrics path")
 }
 
