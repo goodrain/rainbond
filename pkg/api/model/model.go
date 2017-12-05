33afb152b508c8e031a707551fb1b5fb8ef0c8bc
崔斌 <cuiminmm@gmail.com>
2017-12-05 12:00:03 +0800

    [REV]Update image cloud share

--- pkg/api/model/model.go
+++ pkg/api/model/model.go
@@ -1,28 +1,28 @@
-
 // RAINBOND, Application Management Platform
 // Copyright (C) 2014-2017 Goodrain Co., Ltd.
- 
+
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version. For any non-GPL usage of Rainbond,
 // one or multiple Commercial Licenses authorized by Goodrain Co., Ltd.
 // must be obtained first.
- 
+
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
- 
+
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 package model
 
 import (
-	dbmodel "github.com/goodrain/rainbond/pkg/db/model"
 	"net/url"
 	"time"
+
+	dbmodel "github.com/goodrain/rainbond/pkg/db/model"
 )
 
 //ServiceGetCommon path参数
@@ -857,6 +857,7 @@ type PublicShare struct {
 	ShareID    string `json:"share_id" validate:"share_id"`
 	EventID    string `json:"event_id" validate:"event_id"`
 	Dest       string `json:"dest" validate:"dest|in:yb,ys"`
+	ServiceID  string `json:"service_id" validate:"service_id"`
 }
 
 //SlugShare Slug 类型
@@ -864,7 +865,6 @@ type SlugShare struct {
 	PublicShare
 	ServiceKey    string `json:"service_key" validate:"service_key"`
 	APPVersion    string `json:"app_version" validate:"app_version"`
-	ServiceID     string `json:"service_id" validate:"app_version"`
 	DeployVersion string `json:"deploy_version" validate:"deploy_version"`
 	TenantID      string `json:"tenant_id" validate:"tenant_id"`
 	Dest          string `json:"dest" validate:"dest|in:yb,ys"`
