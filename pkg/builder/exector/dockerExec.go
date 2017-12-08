a1e1128576873257c7cbb1c2bdc237cf6723a914
崔斌 <cuiminmm@gmail.com>
2017-12-08 11:09:33 +0800

    [DEL]Delete fmt log

--- pkg/builder/exector/dockerExec.go
+++ pkg/builder/exector/dockerExec.go
@@ -1,31 +1,31 @@
-
 // RAINBOND, Application Management Platform
 // Copyright (C) 2014-2017 Goodrain Co., Ltd.
- 
+
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version. For any non-GPL usage of Rainbond,
 // one or multiple Commercial Licenses authorized by Goodrain Co., Ltd.
 // must be obtained first.
- 
+
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
- 
+
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 package exector
 
 import (
-	"github.com/goodrain/rainbond/pkg/event"
 	"bufio"
 	"context"
 	"fmt"
 	"io"
 	"os/exec"
+
+	"github.com/goodrain/rainbond/pkg/event"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/docker/docker/api/types"
@@ -62,7 +62,7 @@ func ShowExec(command string, params []string, logger ...event.Logger) error {
 			if errL != nil || io.EOF == errL {
 				break
 			}
-			fmt.Print(line)
+			//fmt.Print(line)
 			logrus.Infof(fmt.Sprintf("builder: %v", line))
 			logger[0].Info(fmt.Sprintf("builder:%v", line), map[string]string{"step": "build-exector"})
 		}
