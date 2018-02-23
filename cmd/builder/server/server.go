f7938d2f1333298c9dd064f0fef9a5c60444e3c5
goodrain <zengqg@goodrain.com>
2018-02-23 15:24:53 +0800

    [ADD] worker and builder support pprof

--- cmd/builder/server/server.go
+++ cmd/builder/server/server.go
@@ -1,19 +1,18 @@
-
 // RAINBOND, Application Management Platform
 // Copyright (C) 2014-2017 Goodrain Co., Ltd.
- 
+
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version. For any non-GPL usage of Rainbond,
 // one or multiple Commercial Licenses authorized by Goodrain Co., Ltd.
 // must be obtained first.
- 
+
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
- 
+
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
@@ -36,18 +35,21 @@ limitations under the License.
 */
 
 import (
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/goodrain/rainbond/cmd/builder/option"
 	"github.com/goodrain/rainbond/pkg/builder/discover"
 	"github.com/goodrain/rainbond/pkg/builder/exector"
 	"github.com/goodrain/rainbond/pkg/db"
 	"github.com/goodrain/rainbond/pkg/db/config"
 	"github.com/goodrain/rainbond/pkg/event"
-	"os"
-	"os/signal"
-	"syscall"
+
+	"net/http"
 
 	"github.com/Sirupsen/logrus"
-	"net/http"
 	"github.com/goodrain/rainbond/pkg/builder/api"
 )
 
@@ -82,9 +84,9 @@ func Run(s *option.Builder) error {
 	}
 	defer dis.Stop()
 
-	r:=api.APIServer()
+	r := api.APIServer()
+	logrus.Info("builder api listen port 3228")
 	go http.ListenAndServe(":3228", r)
-
 
 	logrus.Info("builder begin running...")
 	//step finally: listen Signal
