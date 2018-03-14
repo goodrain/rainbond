52d586ad18dcf3bbac3763a98eef5e070456b53e
goodrain <zengqg@goodrain.com>
2018-03-14 14:12:26 +0800

    [REV] update code style about license

--- pkg/mq/api/mq/mq.go
+++ pkg/mq/api/mq/mq.go
@@ -1,5 +1,5 @@
+// Copyright (C) 2014-2018 Goodrain Co., Ltd.
 // RAINBOND, Application Management Platform
-// Copyright (C) 2014-2017 Goodrain Co., Ltd.
  
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
