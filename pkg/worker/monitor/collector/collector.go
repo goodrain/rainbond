b1f9e572bd03c719b0905ea6f50df7f9707e0f09
goodrain <zengqg@goodrain.com>
2018-03-16 18:20:40 +0800

    [FIX] fix billing data duplication bug

--- pkg/worker/monitor/collector/collector.go
+++ pkg/worker/monitor/collector/collector.go
@@ -108,7 +108,7 @@ func (e *Exporter) scrape(ch chan<- prometheus.Metric) {
 	//获取内存使用情况
 	for _, service := range services {
 		if _, ok := status[service.ServiceID]; ok {
-			e.memoryUse.WithLabelValues(service.TenantID, service.ServiceID).Set(float64(service.ContainerMemory * service.Replicas))
+			e.memoryUse.WithLabelValues(service.TenantID, service.ServiceID, "running").Set(float64(service.ContainerMemory * service.Replicas))
 		}
 	}
 	ch <- prometheus.MustNewConstMetric(scrapeDurationDesc, prometheus.GaugeValue, time.Since(scrapeTime).Seconds(), "collect.memory")
@@ -155,7 +155,7 @@ func New(statusManager status.ServiceStatusManager, cache *cache.DiskCache) *Exp
 			Namespace: namespace,
 			Name:      "appmemory",
 			Help:      "tenant service memory used.",
-		}, []string{"tenant_id", "service_id"}),
+		}, []string{"tenant_id", "service_id", "service_status"}),
 		fsUse: prometheus.NewGaugeVec(prometheus.GaugeOpts{
 			Namespace: namespace,
 			Name:      "appfs",
