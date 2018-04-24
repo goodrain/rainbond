696885fad34d13ee0aef669bc7414ee281e0ed4e
zhouyq <zyq916@gmail.com>
2018-04-24 11:39:53 +0800

    [FIX] fix build local hosts file domain resolving

--- hack/contrib/docker/chaos/build.pl
+++ hack/contrib/docker/chaos/build.pl
@@ -25,5 +25,5 @@ if ($envs) {
 chdir($source_dir);
 #system("git archive master | docker run -i --rm -v $cache_dir:/tmp/cache:rw -a stdin -a stdout goodrain.net/builder - >$package_file");
 #system("git archive master | docker run -i --rm -a stdin -a stdout -e SLUG_VERSION=$version -v $slug_dir:/tmp/slug -v $cache_dir:/tmp/cache goodrain.me/builder local >$logfile");
-$cmd="git archive $branch | $docker_bin run -i --rm --name $name -v $cache_dir:/tmp/cache:rw -a stdin -a stdout $APPEND_ENV_STRING -e SLUG_VERSION=$version -e SERVICE_ID=$service_id -e TENANT_ID=$tenant_id -v $slug_dir:/tmp/slug goodrain.me/builder local";
+$cmd="git archive $branch | $docker_bin run -i --net=host --rm --name $name -v $cache_dir:/tmp/cache:rw -a stdin -a stdout $APPEND_ENV_STRING -e SLUG_VERSION=$version -e SERVICE_ID=$service_id -e TENANT_ID=$tenant_id -v $slug_dir:/tmp/slug goodrain.me/builder local";
 system($cmd);
