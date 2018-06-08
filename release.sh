60cd5fc777612dcb2c69050b2fe438b87f876bcb
ysicing <ops.ysicing@gmail.com>
2018-06-08 23:28:13 +0800

    [REV] rename node & grctl

--- release.sh
+++ release.sh
@@ -31,9 +31,9 @@ function build() {
 	echo "rbd plugins version:$release_desc"
 	
 	echo "build node"
-    docker run --rm -v `pwd`:${WORK_DIR} -w ${WORK_DIR} -it golang:1.8.3 go build -ldflags '-w -s'  -o $releasedir/dist/usr/local/bin/${BASE_NAME}-node ./cmd/node
+    docker run --rm -v `pwd`:${WORK_DIR} -w ${WORK_DIR} -it golang:1.8.3 go build -ldflags '-w -s'  -o $releasedir/dist/usr/local/bin/node ./cmd/node
 	echo "build grctl"
-	docker run --rm -v `pwd`:${WORK_DIR} -w ${WORK_DIR} -it golang:1.8.3 go build -ldflags '-w -s'  -o $releasedir/dist/usr/local/bin/${BASE_NAME}-grctl ./cmd/grctl
+	docker run --rm -v `pwd`:${WORK_DIR} -w ${WORK_DIR} -it golang:1.8.3 go build -ldflags '-w -s'  -o $releasedir/dist/usr/local/bin/grctl ./cmd/grctl
 
 	cd $releasedir/dist/usr/local/
 	tar zcf pkg.tgz `find . -maxdepth 1|sed 1d`
