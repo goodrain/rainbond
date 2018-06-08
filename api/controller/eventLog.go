4735a74f73e15470bc7f82a754b92a2da670a155
barnettZQG <zengqg@yiyun.pro>
2018-06-09 00:35:51 +0800

    [FIX] fix get docker log instance error

--- api/controller/eventLog.go
+++ api/controller/eventLog.go
@@ -160,7 +160,7 @@ func (e *EventLogStruct) LogSocket(w http.ResponseWriter, r *http.Request) {
 	serviceID := r.Context().Value(middleware.ContextKey("service_id")).(string)
 	value, err := handler.GetEventHandler().GetLogInstance(serviceID)
 	if err != nil {
-		if strings.Contains(err.Error(), "exist") {
+		if strings.Contains(err.Error(), "Key not found") {
 			httputil.ReturnError(r, w, 404, err.Error())
 			return
 		}
