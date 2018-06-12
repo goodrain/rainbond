a171625f40bce1f0ebae941b590f261d62130921
zhoujunhao <18853925545@163.com>
2018-06-12 13:20:47 +0800

    [ADD] add test code

--- appruntimesync/clean/clean.go
+++ appruntimesync/clean/clean.go
@@ -44,22 +44,24 @@ func (c *CheanManager) Run() {
 	for _, v := range ALLTeantsList {
 		alllist = append(alllist, v.UUID)
 	}
-	
+
 
 	StatefulSets, err := c.kubeclient.StatefulSets("824b2e9dcc4d461a852ddea20369d377").List(meta_v1.ListOptions{})
 	ReplicationControllers, err := c.kubeclient.ReplicationControllers("c69c40ecedae41ca9fbb6c3cec0926f2").List(meta_v1.ListOptions{})
 
-	fmt.Println(StatefulSets.Items[0].Name)
-	fmt.Println(StatefulSets.Items[0].GenerateName)
-	fmt.Println(StatefulSets.Items[0].UID)
-	fmt.Println(StatefulSets.Items[0].Namespace)
-	fmt.Println(StatefulSets.Items[0].Labels)
+	for _,v:=range StatefulSets.Items{
+		fmt.Println(v.Name)
+		fmt.Println(v.Labels)
+	}
+	err2 := c.kubeclient.StatefulSets("824b2e9dcc4d461a852ddea20369d377").Delete("grd1b4e0",meta_v1.NewDeleteOptions(0))
+	if err2!=nil{
+		fmt.Println("错误",err2)
+	}
 	fmt.Println("----------------------")
-	fmt.Println(ReplicationControllers.Items[0].Name)
-	fmt.Println(ReplicationControllers.Items[0].GenerateName)
-	fmt.Println(ReplicationControllers.Items[0].UID)
-	fmt.Println(ReplicationControllers.Items[0].Namespace)
-	fmt.Println(ReplicationControllers.Items[0].Labels)
+	for _,v:=range ReplicationControllers.Items{
+		fmt.Println(v.Name)
+		fmt.Println(v.Labels)
+	}
 
 
 }
