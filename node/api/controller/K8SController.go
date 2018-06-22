64543d4c4181581fdc0e331dbc17280a9aba9ab3
zhoujunhao <18853925545@163.com>
2018-06-22 16:04:30 +0800

    [ADD] add disk usage information

--- node/api/controller/K8SController.go
+++ node/api/controller/K8SController.go
@@ -40,6 +40,7 @@ import (
 	"strconv"
 
 	"github.com/goodrain/rainbond/node/api/model"
+	"syscall"
 )
 
 func GetNodeDetails(w http.ResponseWriter, r *http.Request) {
@@ -320,6 +321,26 @@ func CapRes(w http.ResponseWriter, r *http.Request) {
 	api.ReturnSuccess(r, w, result)
 }
 
+
+type DiskStatus struct {
+	All  uint64 `json:"all"`
+	Used uint64 `json:"used"`
+	Free uint64 `json:"free"`
+}
+
+// disk usage of path/disk
+func DiskUsage(path string) (disk DiskStatus) {
+	fs := syscall.Statfs_t{}
+	err := syscall.Statfs(path, &fs)
+	if err != nil {
+		return
+	}
+	disk.All = fs.Blocks * uint64(fs.Bsize)
+	disk.Free = fs.Bfree * uint64(fs.Bsize)
+	disk.Used = disk.All - disk.Free
+	return
+}
+
 func RegionRes(w http.ResponseWriter, r *http.Request) {
 	nodes, err := nodeService.GetAllNode()
 	if err != nil {
@@ -345,6 +366,7 @@ func RegionRes(w http.ResponseWriter, r *http.Request) {
 			memR += rm
 		}
 	}
+	disk :=DiskUsage("/grdata")
 	podMemRequestMB := memR / 1024 / 1024
 	result := new(model.ClusterResource)
 	result.CapCpu = int(capCpu)
@@ -353,6 +375,10 @@ func RegionRes(w http.ResponseWriter, r *http.Request) {
 	result.ReqMem = int(podMemRequestMB)
 	result.Node = len(nodes)
 	result.Tenant = 0
+	result.CapDisk = disk.All
+	result.ReqDisk = disk.Used
+
+
 	api.ReturnSuccess(r, w, result)
 }
 func UpdateNode(w http.ResponseWriter, r *http.Request) {
