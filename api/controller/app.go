7d97547ecbcaa46b4bbe5965d28d7afd30fca64c
zhoujunhao <18853925545@163.com>
2018-09-25 15:23:36 +0800

    [ADD] add CORS

--- api/controller/app.go
+++ api/controller/app.go
@@ -166,6 +166,11 @@ func (a *AppStruct) Upload(w http.ResponseWriter, r *http.Request) {
 		httputil.ReturnSuccess(r, w, "successful")
 
 	case "OPTIONS":
+		origin := r.Header.Get("Origin")
+		w.Header().Add("Access-Control-Allow-Origin", origin)
+		w.Header().Add("Access-Control-Allow-Methods", "POST,OPTIONS")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
+		w.Header().Add("Access-Control-Allow-Headers", "x-requested-with,content-type,X-Custom-Header")
 		httputil.ReturnSuccess(r, w, nil)
 	}
 
