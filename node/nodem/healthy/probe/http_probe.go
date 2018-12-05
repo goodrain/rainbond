3739b8c0f76901ade3e57fa1cb6b612998b6996b
barnett <576501057@qq.com>
2018-12-05 20:41:40 +0800

    [REV] change http probe method

--- node/nodem/healthy/probe/http_probe.go
+++ node/nodem/healthy/probe/http_probe.go
@@ -2,9 +2,12 @@ package probe
 
 import (
 	"context"
+	"net"
 	"net/http"
-	"strings"
+	"net/url"
 	"time"
+
+	"github.com/Sirupsen/logrus"
 
 	"github.com/goodrain/rainbond/node/nodem/client"
 	"github.com/goodrain/rainbond/node/nodem/service"
@@ -33,6 +36,9 @@ func (h *HttpProbe) Stop() {
 
 //HTTPCheck http check
 func (h *HttpProbe) HTTPCheck() {
+	if h.TimeInterval == 0 {
+		h.TimeInterval = 5
+	}
 	timer := time.NewTimer(time.Second * time.Duration(h.TimeInterval))
 	defer timer.Stop()
 	for {
@@ -52,22 +58,44 @@ func (h *HttpProbe) HTTPCheck() {
 	}
 }
 
+// Return true if the underlying error indicates a http.Client timeout.
+//
+// Use for errors returned from http.Client methods (Get, Post).
+func isClientTimeout(err error) bool {
+	if uerr, ok := err.(*url.Error); ok {
+		if nerr, ok := uerr.Err.(net.Error); ok && nerr.Timeout() {
+			return true
+		}
+	}
+	return false
+}
+
 //GetHTTPHealth get http health
 func GetHTTPHealth(address string) map[string]string {
 	c := &http.Client{
-		Timeout: 5 * time.Second,
+		Timeout: 10 * time.Second,
 	}
-	if !strings.Contains(address, "://") {
-		address = "http://" + address
-	}
-	resp, err := c.Get(address)
+	addr, err := url.Parse(address)
 	if err != nil {
-		return map[string]string{"status": service.Stat_death, "info": "Request service is unreachable"}
+		logrus.Errorf("%s is invalid", address)
+		return map[string]string{"status": service.Stat_healthy, "info": "check url is invalid"}
 	}
-	defer resp.Body.Close()
+	if addr.Scheme == "" {
+		addr.Scheme = "http"
+	}
+	resp, err := c.Get(addr.String())
+	if err != nil {
+		if isClientTimeout(err) {
+			return map[string]string{"status": service.Stat_death, "info": "Request service is unreachable"}
+		}
+		logrus.Errorf("http probe request error %s", err.Error())
+		return map[string]string{"status": service.Stat_unhealthy, "info": err.Error()}
+	}
+	if resp.Body != nil {
+		defer resp.Body.Close()
+	}
 	if resp.StatusCode >= 500 {
 		return map[string]string{"status": service.Stat_unhealthy, "info": "Service unhealthy"}
 	}
 	return map[string]string{"status": service.Stat_healthy, "info": "service health"}
-
 }
--- node/nodem/healthy/probe/http_probe_test.go
+++ node/nodem/healthy/probe/http_probe_test.go
@@ -1,27 +1,45 @@
+// RAINBOND, Application Management Platform
+// Copyright (C) 2014-2017 Goodrain Co., Ltd.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version. For any non-GPL usage of Rainbond,
+// one or multiple Commercial Licenses authorized by Goodrain Co., Ltd.
+// must be obtained first.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 package probe
-//
-//import (
-//	"testing"
-//	"context"
-//	"github.com/goodrain/rainbond/node/nodem/service"
-//	"fmt"
-//)
-//
-//func TestHttpProbe_Check(t *testing.T) {
-//	ctx, cancel := context.WithCancel(context.Background())
-//	resultChannel := make(chan service.HealthStatus,10)
-//	httpProbe := HttpProbe{
-//		name:"builder",
-//		address:"127.0.0.1:3228/v2/builder/health",
-//		ctx:ctx,
-//		cancel:cancel,
-//		resultsChan:resultChannel,
-//	}
-//	go httpProbe.Check()
-//	for  {
-//		result := <- resultChannel
-//		fmt.Println(result.Name,result.Status,result.Info)
-//	}
-//}
 
+import (
+	"context"
+	"fmt"
+	"testing"
 
+	"github.com/goodrain/rainbond/node/nodem/service"
+)
+
+func TestHttpProbe_Check(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	resultChannel := make(chan *service.HealthStatus, 10)
+	httpProbe := HttpProbe{
+		Name:         "builder",
+		Address:      "https://baidu.com/sadasd",
+		Ctx:          ctx,
+		Cancel:       cancel,
+		ResultsChan:  resultChannel,
+		TimeInterval: 3,
+	}
+	go httpProbe.Check()
+	for {
+		result := <-resultChannel
+		fmt.Println(result.Name, result.Status, result.Info)
+	}
+}
