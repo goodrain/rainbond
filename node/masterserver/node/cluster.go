0cb9af2c3d3c95a73ea317a00f4ea829a8e2a9aa
barnett <576501057@qq.com>
2018-12-25 18:23:05 +0800

    [REV] auto set node unscheduler only node number more than 1

--- node/masterserver/node/cluster.go
+++ node/masterserver/node/cluster.go
@@ -153,6 +153,10 @@ func (n *Cluster) GetNode(id string) *client.HostNode {
 	}
 	return nil
 }
+func (n *Cluster) getKubeNodeCount() int {
+	kubeNodes, _ := n.kubecli.GetNodes()
+	return len(kubeNodes)
+}
 
 //handleNodeStatus Master integrates node status and kube node status
 func (n *Cluster) handleNodeStatus(v *client.HostNode) {
@@ -219,10 +223,14 @@ func (n *Cluster) handleNodeStatus(v *client.HostNode) {
 		for _, action := range v.NodeStatus.AdviceAction {
 			if action == "unscheduler" {
 				if v.NodeStatus.KubeNode != nil && !v.NodeStatus.KubeNode.Spec.Unschedulable {
-					logrus.Infof("node %s is advice set unscheduler,will do this action", v.ID)
-					_, err := n.kubecli.CordonOrUnCordon(v.ID, true)
-					if err != nil {
-						logrus.Errorf("auto set node is unscheduler failure.")
+					if n.getKubeNodeCount() > 1 {
+						logrus.Infof("node %s is advice set unscheduler,will do this action", v.ID)
+						_, err := n.kubecli.CordonOrUnCordon(v.ID, true)
+						if err != nil {
+							logrus.Errorf("auto set node is unscheduler failure.")
+						}
+					} else {
+						logrus.Warning("node %s is advice set unscheduler,but only have one node,can not do it", v.ID)
 					}
 				}
 			}
