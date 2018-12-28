f6624ee8bed20b338cfe7cc37ff7e2026effc82b
GLYASAI <chaoschunqfeijg@163.com>
2018-12-28 09:10:13 +0800

    [REV] stop verifying clientSet

--- gateway/controller/util.go
+++ gateway/controller/util.go
@@ -4,7 +4,6 @@ import (
 	"github.com/Sirupsen/logrus"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // NewClientSet returns a new kubernetes clientSet
@@ -20,10 +19,10 @@ func NewClientSet(kubeconfig string) (*kubernetes.Clientset, error) {
 		return nil, err
 	}
 
-	_, err = clientSet.CoreV1().Namespaces().List(v1.ListOptions{})
-	if err != nil {
-		return nil, err
-	}
+	//_, err = clientSet.CoreV1().Namespaces().List(v1.ListOptions{})
+	//if err != nil {
+	//	return nil, err
+	//}
 
 	logrus.Debug("Kube client api create success.")
 
