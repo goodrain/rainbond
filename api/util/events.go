3b4f50b325fcc57247051b5c22b2b73934ce0b6d
凡羊羊 <a985850124@qq.com>
2019-09-10 12:53:50 +0800

    modify event.request_body size to 1024

--- api/util/events.go
+++ api/util/events.go
@@ -82,8 +82,8 @@ func checkTimeout(event *dbmodel.ServiceEvent) bool {
 
 // CreateEvent save event
 func CreateEvent(target, optType, targetID, tenantID, reqBody, userName string, synType int) (*dbmodel.ServiceEvent, error) {
-	if len(reqBody) > 255 {
-		reqBody = reqBody[0:255]
+	if len(reqBody) > 1024 {
+		reqBody = reqBody[0:1024]
 	}
 	event := dbmodel.ServiceEvent{
 		EventID:     util.NewUUID(),
