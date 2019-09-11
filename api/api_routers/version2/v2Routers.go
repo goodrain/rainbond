a41bd32c2b60c2822c2577216b69e6bc4b66420d
凡羊羊 <a985850124@qq.com>
2019-09-11 16:08:38 +0800

    modify share event as syn

--- api/api_routers/version2/v2Routers.go
+++ api/api_routers/version2/v2Routers.go
@@ -159,7 +159,7 @@ func (v2 *V2) serviceRouter() chi.Router {
 	r.Get("/deployversion", controller.GetManager().GetDeployVersion)
 	r.Delete("/build-version/{build_version}", middleware.WrapEL(controller.GetManager().BuildVersionInfo, dbmodel.TargetTypeService, "delete-buildversion", dbmodel.SYNEVENTTYPE))
 	//应用分享
-	r.Post("/share", middleware.WrapEL(controller.GetManager().Share, dbmodel.TargetTypeService, "share-service", dbmodel.ASYNEVENTTYPE))
+	r.Post("/share", middleware.WrapEL(controller.GetManager().Share, dbmodel.TargetTypeService, "share-service", dbmodel.SYNEVENTTYPE))
 	r.Get("/share/{share_id}", controller.GetManager().ShareResult)
 	//应用日志相关
 	r.Post("/log", controller.GetManager().Logs)
