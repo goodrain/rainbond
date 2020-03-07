4918c66a2b9223483144ee1434f05e6d93596404
GLYASAI <chaoschunqfeijg@163.com>
2020-03-07 12:03:51 +0800

    make image repo configurable

--- builder/exector/plugin_image.go
+++ builder/exector/plugin_image.go
@@ -20,20 +20,19 @@ package exector
 
 import (
 	"fmt"
+	"path"
 	"strings"
 
 	"github.com/goodrain/rainbond/builder"
+	"github.com/goodrain/rainbond/builder/model"
+	"github.com/goodrain/rainbond/builder/parser"
 	"github.com/goodrain/rainbond/builder/sources"
-
 	"github.com/goodrain/rainbond/db"
 	"github.com/goodrain/rainbond/event"
-
+	"github.com/goodrain/rainbond/mq/api/grpc/pb"
 	"github.com/pquerna/ffjson/ffjson"
 
-	"github.com/goodrain/rainbond/builder/model"
-
 	"github.com/Sirupsen/logrus"
-	"github.com/goodrain/rainbond/mq/api/grpc/pb"
 )
 
 func (e *exectorManager) pluginImageBuild(task *pb.TaskMessage) {
@@ -71,8 +70,10 @@ func (e *exectorManager) pluginImageBuild(task *pb.TaskMessage) {
 }
 
 func (e *exectorManager) run(t *model.BuildPluginTaskBody, logger event.Logger) error {
+	imagep := parser.ParseImageName(t.ImageURL)
+	image := path.Join(builder.REGISTRYDOMAIN, imagep.GetSimpleName()) + ":" + imagep.Tag
 
-	if _, err := sources.ImagePull(e.DockerClient, t.ImageURL, t.ImageInfo.HubUser, t.ImageInfo.HubPassword, logger, 10); err != nil {
+	if _, err := sources.ImagePull(e.DockerClient, image, t.ImageInfo.HubUser, t.ImageInfo.HubPassword, logger, 10); err != nil {
 		logrus.Errorf("pull image %v error, %v", t.ImageURL, err)
 		logger.Error("拉取镜像失败", map[string]string{"step": "builder-exector", "status": "failure"})
 		return err
--- builder/repostory.go
+++ builder/repostory.go
@@ -20,6 +20,7 @@ package builder
 
 import (
 	"os"
+	"path"
 
 	"github.com/goodrain/rainbond/util/constants"
 )
@@ -34,12 +35,17 @@ func init() {
 	if os.Getenv("BUILD_IMAGE_REPOSTORY_PASS") != "" {
 		REGISTRYPASS = os.Getenv("BUILD_IMAGE_REPOSTORY_PASS")
 	}
+	RUNNERIMAGENAME = "/runner"
 	if os.Getenv("RUNNER_IMAGE_NAME") != "" {
 		RUNNERIMAGENAME = os.Getenv("RUNNER_IMAGE_NAME")
 	}
+	RUNNERIMAGENAME = path.Join(REGISTRYDOMAIN, RUNNERIMAGENAME)
+	BUILDERIMAGENAME = "builder"
 	if os.Getenv("BUILDER_IMAGE_NAME") != "" {
 		BUILDERIMAGENAME = os.Getenv("BUILDER_IMAGE_NAME")
 	}
+
+	BUILDERIMAGENAME = path.Join(REGISTRYDOMAIN, BUILDERIMAGENAME)
 }
 
 //REGISTRYDOMAIN REGISTRY_DOMAIN
@@ -52,7 +58,7 @@ var REGISTRYUSER = ""
 var REGISTRYPASS = ""
 
 //RUNNERIMAGENAME runner image name
-var RUNNERIMAGENAME = REGISTRYDOMAIN + "/runner"
+var RUNNERIMAGENAME string
 
 //BUILDERIMAGENAME builder image name
-var BUILDERIMAGENAME = REGISTRYDOMAIN + "/builder"
+var BUILDERIMAGENAME string
