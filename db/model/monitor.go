7a56bf0646f9ca43c85edab86abf0a42ad04b423
yangk <yangk@xuhaoyudeMacBook-Pro.local>
2020-11-12 18:52:50 +0800

    modified unique index

--- db/model/monitor.go
+++ db/model/monitor.go
@@ -3,9 +3,9 @@ package model
 //TenantServiceMonitor custom service monitor
 type TenantServiceMonitor struct {
 	Model
-	TenantID        string `gorm:"column:tenant_id;size:40;primary_key" json:"tenant_id"`
+	TenantID        string `gorm:"column:tenant_id;size:40;unique_index:unique_tenant_id_name" json:"tenant_id"`
 	ServiceID       string `gorm:"column:service_id;size:40" json:"service_id"`
-	Name            string `gorm:"column:name;size:40;primary_key" json:"name"`
+	Name            string `gorm:"column:name;size:40;unique_index:unique_tenant_id_name" json:"name"`
 	ServiceShowName string `gorm:"column:service_show_name" json:"service_show_name"`
 	Port            int    `gorm:"column:port;size:5" json:"port"`
 	Path            string `gorm:"column:path;size:255" json:"path"`
