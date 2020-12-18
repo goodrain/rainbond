79daddcbe767d1f6c4ea752c474b7485ccf8967a
barnettZQG <zengqg@goodrain.com>
2020-12-18 16:49:11 +0800

    change code

--- monitor/callback/cadvisor.go
+++ monitor/callback/cadvisor.go
@@ -116,6 +116,13 @@ func (c *Cadvisor) toScrape() *prometheus.ScrapeConfig {
 				TargetLabel:  "service_id",
 				Replacement:  "${1}",
 			},
+			{
+				SourceLabels: []model.LabelName{"name"},
+				//k8s_POD_709dfaa8d9b9498a827fd5c503e0d1a1-deployment-8679ff667-j8fj8_5201d8a00fa743c18eb6553778f77c84_d6670db0-00a7-4d2c-a92e-18a19541268d_0
+				Regex:       prometheus.MustNewRegexp("k8s_POD_(.*)-deployment-(.*)"),
+				TargetLabel: "service_id",
+				Replacement: "${1}",
+			},
 		},
 	}
 }
