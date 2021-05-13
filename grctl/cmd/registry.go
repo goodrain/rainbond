f7794ef2a1b7758eb545a6261595d483bba7d25d
GLYASAI <chaoschunqfeijg@163.com>
2021-05-13 17:48:45 +0800

    get config from cluster

--- grctl/cmd/registry.go
+++ grctl/cmd/registry.go
@@ -19,11 +19,18 @@
 package cmd
 
 import (
+	"context"
+	"fmt"
+
+	rainbondv1alpha1 "github.com/goodrain/rainbond-operator/api/v1alpha1"
 	"github.com/goodrain/rainbond/db"
 	"github.com/goodrain/rainbond/db/config"
+	"github.com/goodrain/rainbond/grctl/clients"
 	"github.com/goodrain/rainbond/grctl/registry"
 	"github.com/pkg/errors"
 	"github.com/urfave/cli"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 // NewCmdRegistry registry cmd
@@ -33,7 +40,7 @@ func NewCmdRegistry() cli.Command {
 		Usage: "grctl registry [command]",
 		Subcommands: []cli.Command{
 			{
-				Name:  "cleanup",
+				Name: "cleanup",
 				Usage: `Clean up free images in the registry.
 	The command 'grctl registry cleanup' will delete the index of free images in registry.
 	Then you have to exec the command below to remove blobs from the filesystem:
@@ -42,39 +49,36 @@ func NewCmdRegistry() cli.Command {
 				`,
 				Flags: []cli.Flag{
 					cli.StringFlag{
-						Name:  "dsn",
-						Usage: "The DSN string to connect the database",
-					},
-					cli.StringFlag{
-						Name:  "url",
-						Value: "goodrain.me",
-						Usage: "The url of registry",
-					},
-					cli.StringFlag{
-						Name:  "username",
-						Value: "admin",
-						Usage: "The username of registry",
-					},
-					cli.StringFlag{
-						Name:  "password",
-						Usage: "The password of registry",
+						Name:   "namespace, ns",
+						Usage:  "rainbond namespace",
+						EnvVar: "RBDNamespace",
+						Value:  "rbd-system",
 					},
 				},
 				Action: func(c *cli.Context) error {
 					Common(c)
 
+					namespace := c.String("namespace")
+					var cluster rainbondv1alpha1.RainbondCluster
+					if err := clients.RainbondKubeClient.Get(context.Background(), types.NamespacedName{Namespace: namespace, Name: "rainbondcluster"}, &cluster); err != nil {
+						return errors.Wrap(err, "get configuration from rainbond cluster")
+					}
+
+					dsn, err := databaseDSN(&cluster)
+					if err != nil {
+						return errors.Wrap(err, "get database dsn")
+					}
+
 					dbCfg := config.Config{
-						MysqlConnectionInfo: c.String("dsn"),
+						MysqlConnectionInfo: dsn,
 						DBType:              "mysql",
 					}
 					if err := db.CreateManager(dbCfg); err != nil {
 						return errors.Wrap(err, "create database manager")
 					}
 
-					url := c.String("url")
-					username := c.String("username")
-					password := c.String("password")
-					cleaner, err := registry.NewRegistryCleaner(url, username, password)
+					registryConfig := cluster.Spec.ImageHub
+					cleaner, err := registry.NewRegistryCleaner(registryConfig.Domain, registryConfig.Username, registryConfig.Password)
 					if err != nil {
 						return errors.WithMessage(err, "create registry cleaner")
 					}
@@ -87,4 +91,37 @@ func NewCmdRegistry() cli.Command {
 		},
 	}
 	return c
+}
+
+func databaseDSN(rainbondcluster *rainbondv1alpha1.RainbondCluster) (string, error) {
+	database := rainbondcluster.Spec.RegionDatabase
+	if database != nil {
+		return fmt.Sprintf("%s:%s@tcp(%s)/%s", database.Username, database.Password, database.Host, database.Name), nil
+	}
+	// default name of rbd-db pod is rbd-db-0
+	pod, err := clients.K8SClient.CoreV1().Pods(rainbondcluster.Namespace).Get(context.Background(), "rbd-db-0", metav1.GetOptions{})
+	if err != nil {
+		return "", errors.Wrap(err, "get pod rbd-db-0")
+	}
+	host := pod.Status.PodIP
+	name := "region"
+	for _, ct := range pod.Spec.Containers {
+		if ct.Name != "rbd-db" {
+			continue
+		}
+		for _, env := range ct.Env {
+			if env.Name == "MYSQL_DATABASE" {
+				name = env.Value
+			}
+		}
+	}
+
+	secret, err := clients.K8SClient.CoreV1().Secrets(rainbondcluster.Namespace).Get(context.Background(), "rbd-db", metav1.GetOptions{})
+	if err != nil {
+		return "", errors.Wrap(err, "get secret rbd-db")
+	}
+	username := string(secret.Data["mysql-user"])
+	password := string(secret.Data["mysql-password"])
+
+	return fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", username, password, host, name), nil
 }
