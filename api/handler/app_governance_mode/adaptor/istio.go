749cbafdfd170e40ecbd493d399c77d1a3220481
ZhangSetSail <zqh15131121078@126.com>
2022-03-25 16:57:54 +0800

    istio bug

--- api/handler/app_governance_mode/adaptor/istio.go
+++ api/handler/app_governance_mode/adaptor/istio.go
@@ -2,10 +2,11 @@ package adaptor
 
 import (
 	"context"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	clientset "k8s.io/client-go/kubernetes"
 	"os"
 	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clientset "k8s.io/client-go/kubernetes"
 )
 
 type istioServiceMeshMode struct {
@@ -25,9 +26,9 @@ func (i *istioServiceMeshMode) IsInstalledControlPlane() bool {
 	defer cancel()
 	cmName := os.Getenv("ISTIO_CM")
 	if cmName == "" {
-		cmName = "istio-ca-root-cert"
+		cmName = "istio-sidecar-injector"
 	}
-	_, err := i.kubeClient.CoreV1().ConfigMaps("default").Get(ctx, cmName, metav1.GetOptions{})
+	_, err := i.kubeClient.AdmissionregistrationV1().MutatingWebhookConfigurations().Get(ctx, cmName, metav1.GetOptions{})
 	if err != nil {
 		return false
 	}
