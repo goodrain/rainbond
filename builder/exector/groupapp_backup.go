1dfecc2647a36f1637a3f401c8d2c297195cbd2e
yangk <yangk@goodrain.com>
2022-06-30 16:50:11 +0800

    fix: incorrect version of MediaTypeManifest makes it backup failed

--- builder/exector/groupapp_backup.go
+++ builder/exector/groupapp_backup.go
@@ -334,7 +334,13 @@ func (b *BackupAPPNew) checkVersionExist(version *dbmodel.VersionInfo) (bool, er
 		_, err = reg.Manifest(imageInfo.Name, imageInfo.Tag)
 		if err != nil {
 			logrus.Errorf("get image %s manifest info failure [%v], it could be not exist", version.DeliveredPath, err)
-			return false, err
+			// Compatible with MediaTypeManifest
+			_, err := reg.ManifestV2(imageInfo.Name, imageInfo.Tag)
+			if err != nil {
+				logrus.Errorf("get image %s manifestV2 info failure [%v], it could be not exist", version.DeliveredPath, err)
+				return false, err
+			}
+			return true, nil
 		}
 		return true, nil
 	}
