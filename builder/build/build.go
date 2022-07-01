5770d09ed7e0033406236688b0f19b461a0dec2b
pescox <pescoding@outlook.com>
2022-07-01 10:20:09 +0800

    add commnet on ImageBuildNetworkModeHost

--- builder/build/build.go
+++ builder/build/build.go
@@ -21,8 +21,9 @@ package build
 import (
 	"context"
 	"fmt"
-	"github.com/goodrain/rainbond/db"
 	"strings"
+
+	"github.com/goodrain/rainbond/db"
 
 	"github.com/goodrain/rainbond/builder"
 	"github.com/goodrain/rainbond/builder/parser/code"
@@ -67,7 +68,7 @@ var ImageMediumType MediumType = "image"
 //SlugMediumType slug type
 var SlugMediumType MediumType = "slug"
 
-//ImageBuildNetworkModeHost
+//ImageBuildNetworkModeHost use host network mode during docker build
 var ImageBuildNetworkModeHost = "host"
 
 //Response build result
