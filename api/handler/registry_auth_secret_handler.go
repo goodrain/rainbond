f87856e3defd8ae6fd67b025a3f287dd61a47232
dp <dp.io@outlook.com>
2022-07-28 18:27:57 +0800

    feat(api,chaos,worker): support FROM private image repository for dockerfile building

--- api/handler/registry_auth_secret_handler.go
+++ api/handler/registry_auth_secret_handler.go
@@ -15,13 +15,14 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 package handler
 
 import (
 	apimodel "github.com/goodrain/rainbond/api/model"
 )
 
-//GatewayHandler gateway api handler
+//RegistryAuthSecretHandler registry auth secret handler
 type RegistryAuthSecretHandler interface {
 	AddOrUpdateRegistryAuthSecret(req *apimodel.AddOrUpdateRegistryAuthSecretStruct) error
 	DeleteRegistryAuthSecret(req *apimodel.DeleteRegistryAuthSecretStruct) error
