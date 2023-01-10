db654cd7103f25ddd1ee294738e8aebad84bcc5c
yangk <yangk@goodrain.com>
2023-01-10 16:14:09 +0800

    feat: support create servicemesh cr --temp

--- api/controller/manager.go
+++ api/controller/manager.go
@@ -30,7 +30,7 @@ import (
 	"github.com/goodrain/rainbond/worker/client"
 )
 
-//V2Manager v2 manager
+// V2Manager v2 manager
 type V2Manager interface {
 	Show(w http.ResponseWriter, r *http.Request)
 	Health(w http.ResponseWriter, r *http.Request)
@@ -56,18 +56,18 @@ type V2Manager interface {
 
 var defaultV2Manager V2Manager
 
-//CreateV2RouterManager 创建manager
+// CreateV2RouterManager 创建manager
 func CreateV2RouterManager(conf option.Config, statusCli *client.AppRuntimeSyncClient) (err error) {
 	defaultV2Manager, err = NewManager(conf, statusCli)
 	return err
 }
 
-//GetManager 获取管理器
+// GetManager 获取管理器
 func GetManager() V2Manager {
 	return defaultV2Manager
 }
 
-//NewManager new manager
+// NewManager new manager
 func NewManager(conf option.Config, statusCli *client.AppRuntimeSyncClient) (*V2Routes, error) {
 	etcdClientArgs := &etcdutil.ClientArgs{
 		Endpoints: conf.EtcdEndpoint,
--- api/model/model.go
+++ api/model/model.go
@@ -2169,3 +2169,13 @@ type RainbondPlugins struct {
 	Author      string `json:"author"`
 	Status      string `json:"status"`
 }
+
+type CreateUpdateGovernanceModeReq struct {
+	Provisioner string `json:"provisioner" validate:"required"`
+}
+
+type GovernanceMode struct {
+	Name        string `json:"name"`
+	IsDefault   bool   `json:"is_default"`
+	Description string `json:"description"`
+}
