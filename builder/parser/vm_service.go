801198b136ace96a8536a66b9c167058c0a08515
张启航 <zhangqh@goodrain.com>
2023-11-24 16:41:16 +0800

    perf: virtual machine image source detection

--- builder/parser/vm_service.go
+++ builder/parser/vm_service.go
@@ -21,6 +21,12 @@ package parser
 import (
 	"github.com/goodrain/rainbond/builder/parser/discovery"
 	"github.com/goodrain/rainbond/event"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net/http"
+	"path"
+	"path/filepath"
+	"strings"
 )
 
 // VMServiceParse is one of the implematation of parser.Parser
@@ -43,6 +49,47 @@ func CreateVMServiceParse(sourceBody string, logger event.Logger) Parser {
 
 // Parse blablabla
 func (t *VMServiceParse) Parse() ParseErrorList {
+	var fileExt string
+	if strings.HasPrefix(t.sourceBody, "/grdata") {
+		fileInfoList, err := ioutil.ReadDir(t.sourceBody)
+		if err != nil {
+			logrus.Errorf("read package path %v failure: %v", t.sourceBody, err)
+			t.errappend(Errorf(FatalError, "http get failure"))
+			return t.errors
+		}
+		if len(fileInfoList) != 1 {
+			logrus.Errorf("the current directory contains multiple files: %v", t.sourceBody)
+			t.logger.Error("镜像只可以拥有一个，当前上传了多个文件", map[string]string{"step": "parse"})
+			t.errappend(Errorf(FatalError, "http get failure"))
+			return t.errors
+		}
+		fileExt = path.Ext(fileInfoList[0].Name())
+	} else {
+		rsp, err := http.Get(t.sourceBody)
+		if err != nil {
+			logrus.Errorf("http get %v failure: %v", t.sourceBody, err)
+			t.errappend(Errorf(FatalError, "http get failure"))
+			return t.errors
+		}
+		if rsp.StatusCode != http.StatusOK {
+			logrus.Errorf("url %v cannot be accessed", t.sourceBody)
+			t.logger.Error("镜像下载地址不可用", map[string]string{"step": "parse"})
+			t.errappend(Errorf(FatalError, "url address cannot be accessed"))
+			return t.errors
+		}
+		defer func() {
+			_ = rsp.Body.Close()
+		}()
+
+		baseURL := filepath.Base(t.sourceBody)
+		fileName := strings.Split(baseURL, "?")[0]
+		fileExt = path.Ext(fileName)
+	}
+	if fileExt != ".iso" && fileExt != ".qcow2" && fileExt != ".img" && fileExt != ".tar" && fileExt != ".gz" && fileExt != ".xz" {
+		t.logger.Error("上传包格式校验失败，不符合包要求", map[string]string{"step": "parse"})
+		t.errappend(Errorf(FatalError, "image package format verification failed"))
+		return t.errors
+	}
 	return []ParseError{}
 }
 
@@ -58,4 +105,8 @@ func (t *VMServiceParse) GetServiceInfo() []ServiceInfo {
 // GetImage is a dummy method. there is no image for Third-party service.
 func (t *VMServiceParse) GetImage() Image {
 	return Image{}
+}
+
+func (t *VMServiceParse) errappend(pe ParseError) {
+	t.errors = append(t.errors, pe)
 }
