0278059502dfd31918c1a9d8881b568c40761026
张启航 <101104760+ZhangSetSail@users.noreply.github.com>
2023-12-19 23:41:27 +0800

    fix: component probe create failure

--- worker/appm/conversion/version.go
+++ worker/appm/conversion/version.go
@@ -865,7 +865,7 @@ func createPorts(as *v1.AppService, dbmanager db.Manager) (ports []corev1.Contai
 
 func createProbe(as *v1.AppService, dbmanager db.Manager, mode string) *corev1.Probe {
 	if mode == "liveness" {
-		var probe *corev1.Probe
+		probe := new(corev1.Probe)
 		probeAttribute, err := dbmanager.ComponentK8sAttributeDao().GetByComponentIDAndName(as.ServiceID, model.K8sAttributeNameLiveNessProbe)
 		if probeAttribute != nil && probeAttribute.AttributeValue != "" {
 			err = yaml.Unmarshal([]byte(probeAttribute.AttributeValue), probe)
@@ -876,7 +876,7 @@ func createProbe(as *v1.AppService, dbmanager db.Manager, mode string) *corev1.P
 			return probe
 		}
 	} else {
-		var probe *corev1.Probe
+		probe := new(corev1.Probe)
 		probeAttribute, err := dbmanager.ComponentK8sAttributeDao().GetByComponentIDAndName(as.ServiceID, model.K8sAttributeNameReadinessProbe)
 		if probeAttribute != nil && probeAttribute.AttributeValue != "" {
 			err = yaml.Unmarshal([]byte(probeAttribute.AttributeValue), probe)
