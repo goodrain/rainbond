a5ab8815211691eb61fff25e08ec992b33f545c0
逆流而上 <1666888816@qq.com>
2024-01-09 16:32:16 +0800

    perf: optimize communication exceptions

--- release.sh
+++ release.sh
@@ -93,7 +93,7 @@ build::binary() {
 		if [ ! $(which upx) ]; then
 		  sudo apt-get install -y upx
 		fi
- 	  	sudo upx --best --lzma "${OUTPATH}"
+# 	  	sudo upx --best --lzma "${OUTPATH}"
 #	elif [ "$GOARCH" = "arm64" ]; then
 #		wget https://rainbond-pkg.oss-cn-shanghai.aliyuncs.com/upx/upx-4.0.2-arm64_linux/upx
 #		chmod +x upx
