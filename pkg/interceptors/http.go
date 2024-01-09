f3888a046249e8c6bc3b17f7e3096470b7dfbc69
逆流而上 <1666888816@qq.com>
2024-01-09 10:22:32 +0800

    perf: optimize communication exceptions

--- pkg/interceptors/http.go
+++ pkg/interceptors/http.go
@@ -1,6 +1,7 @@
 package interceptors
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi/middleware"
 	"github.com/goodrain/rainbond/pkg/component/etcd"
@@ -9,13 +10,14 @@ import (
 	"strings"
 )
 
+// Recoverer -
 func Recoverer(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if rvr := recover(); rvr != nil && rvr != http.ErrAbortHandler {
 				// Check if the panic is a nil pointer exception
-				if isNilPointerException(rvr) && etcd.Default().EtcdClient == nil {
-					w.WriteHeader(http.StatusServiceUnavailable)
+				if isNilPointerException(rvr) {
+					handleServiceUnavailable(w, r)
 					return
 				}
 
@@ -37,7 +39,7 @@ func Recoverer(next http.Handler) http.Handler {
 	return http.HandlerFunc(fn)
 }
 
-// Check if the panic is a nil pointer exception
+// isNilPointerException Check if the panic is a nil pointer exception
 func isNilPointerException(p interface{}) bool {
 	if p == nil {
 		return false
@@ -45,4 +47,34 @@ func isNilPointerException(p interface{}) bool {
 
 	errMsg := fmt.Sprintf("%v", p)
 	return strings.Contains(errMsg, "runtime error: invalid memory address or nil pointer dereference")
+}
+
+// handleServiceUnavailable -
+func handleServiceUnavailable(w http.ResponseWriter, r *http.Request) {
+	// Additional information about why etcd service is not available
+	errorMessage := "部分服务不可用"
+
+	if etcd.Default().EtcdClient == nil {
+		errorMessage = "Etcd 服务不可用"
+	}
+
+	// Create a response JSON
+	response := map[string]interface{}{
+		"error": errorMessage,
+	}
+
+	// Convert the response to JSON
+	responseJSON, err := json.Marshal(response)
+	if err != nil {
+		// Handle JSON marshaling error
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Set appropriate headers
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusServiceUnavailable)
+
+	// Write the JSON response to the client
+	_, _ = w.Write(responseJSON)
 }
