35aedacb77e577f796f8bdb0f62612cacfec4d85
逆流而上 <1666888816@qq.com>
2024-01-16 11:26:19 +0800

    feat: fix monitor

--- hack/contrib/docker/monitor/entrypoint.sh
+++ hack/contrib/docker/monitor/entrypoint.sh
@@ -4,5 +4,5 @@ if [ "$1" = "bash" ];then
 elif [ "$1" = "version" ];then
     /run/rainbond-monitor version
 else
-    exec /bin/tini -- /run/rainbond-monitor $@
+    exec /run/rainbond-monitor $@
 fi
