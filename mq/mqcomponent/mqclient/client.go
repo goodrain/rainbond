c5d64aed48c4b4ded7d4847d885d8e0d235fcf99
逆流而上 <1666888816@qq.com>
2024-02-18 18:52:14 +0800

    fix: pass ci
    Signed-off-by: 逆流而上 <1666888816@qq.com>

--- mq/mqcomponent/mqclient/client.go
+++ mq/mqcomponent/mqclient/client.go
@@ -8,25 +8,32 @@ import (
 
 var defaultMQClientComponent *Component
 
+// Component -
 type Component struct {
 	actionMQ mq.ActionMQ
 }
 
+// Default -
 func Default() *Component {
 	return defaultMQClientComponent
 }
+
+// ActionMQ -
 func (m *Component) ActionMQ() mq.ActionMQ {
 	return m.actionMQ
 }
 
+// Start -
 func (m *Component) Start(ctx context.Context, cfg *configs.Config) error {
 	m.actionMQ = mq.NewActionMQ(ctx, cfg.MQConfig)
 	return m.actionMQ.Start()
 }
 
+// CloseHandle -
 func (m *Component) CloseHandle() {
 }
 
+// MQClient -
 func MQClient() *Component {
 	defaultMQClientComponent = &Component{}
 	return defaultMQClientComponent
