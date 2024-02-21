61953e467fa92bb31934dba6e6e13fe4686e26b4
逆流而上 <1666888816@qq.com>
2024-02-21 18:15:56 +0800

    perf: reduce rbd-builder,rbd-worker dependency on etcd
    Signed-off-by: 逆流而上 <1666888816@qq.com>

--- cmd/api/option/option.go
+++ cmd/api/option/option.go
@@ -39,9 +39,6 @@ type Config struct {
 	BuilderAPI             []string
 	V1API                  string
 	MQAPI                  string
-	EtcdCaFile             string
-	EtcdCertFile           string
-	EtcdKeyFile            string
 	APISSL                 bool
 	APICertFile            string
 	APIKeyFile             string
@@ -100,9 +97,6 @@ func (a *APIServer) AddFlags(fs *pflag.FlagSet) {
 	fs.StringVar(&a.V1API, "v1-api", "127.0.0.1:8887", "the region v1 api")
 	fs.StringSliceVar(&a.BuilderAPI, "builder-api", []string{"rbd-chaos:3228"}, "the builder api")
 	fs.BoolVar(&a.StartRegionAPI, "start", false, "Whether to start region old api")
-	fs.StringVar(&a.EtcdCaFile, "etcd-ca", "", "verify etcd certificates of TLS-enabled secure servers using this CA bundle")
-	fs.StringVar(&a.EtcdCertFile, "etcd-cert", "", "identify secure etcd client using this TLS certificate file")
-	fs.StringVar(&a.EtcdKeyFile, "etcd-key", "", "identify secure etcd client using this TLS key file")
 	fs.StringVar(&a.Opentsdb, "opentsdb", "127.0.0.1:4242", "opentsdb server config")
 	fs.StringVar(&a.RegionTag, "region-tag", "test-ali", "region tag setting")
 	fs.StringVar(&a.LoggerFile, "logger-file", "/logs/request.log", "request log file path")
