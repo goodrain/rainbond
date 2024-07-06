3aa7d9f011f17edcb2822057f4c8e6dab3c71a92
张启航 <101104760+ZhangSetSail@users.noreply.github.com>
2024-07-06 18:49:36 +0800

    perf: add logger err
    Signed-off-by: 张启航 <101104760+ZhangSetSail@users.noreply.github.com>

--- builder/exector/service_check.go
+++ builder/exector/service_check.go
@@ -95,7 +95,8 @@ func (e *exectorManager) serviceCheck(task *pb.TaskMessage) {
 		if r := recover(); r != nil {
 			logrus.Errorf("service check error: %v", r)
 			debug.PrintStack()
-			logger.Error("The back-end service has deserted, please try again.", map[string]string{"step": "callback", "status": "failure"})
+			errRet := fmt.Sprintf("The back-end service has deserted, please try again. %v", r)
+			logger.Error(errRet, map[string]string{"step": "callback", "status": "failure"})
 		}
 	}()
 	logger.Info("Start component deploy source check.", map[string]string{"step": "starting"})
