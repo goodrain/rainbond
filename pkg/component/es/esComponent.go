4bf94da2d86ad2e269eafe5256317ed053403abe
yangk <yangk@goodrain.com>
2024-07-25 09:14:53 +0800

    perf: support configrue es and http connections
    Signed-off-by: yangk <yangk@goodrain.com>

--- pkg/component/es/esComponent.go
+++ pkg/component/es/esComponent.go
@@ -8,6 +8,9 @@ import (
 	"github.com/sirupsen/logrus"
 	"io"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
 var defaultEsComponent *Component
@@ -20,15 +23,39 @@ type Component struct {
 	client   *http.Client
 }
 
+// createHTTPClient - 创建带有连接池配置的 HTTP 客户端
+func createHTTPClient() *http.Client {
+	maxIdleConns := 100
+	if os.Getenv("HTTP_MAX_IDLE_CONNS") != "" {
+		idleCon, err := strconv.Atoi(os.Getenv("HTTP_MAX_IDLE_CONNS"))
+		if err == nil {
+			maxIdleConns = idleCon
+		}
+	}
+	maxIdleConnsPerHost := 100
+	if os.Getenv("HTTP_MAX_IDLE_CONNS_PER_HOST") != "" {
+		idleCon, err := strconv.Atoi(os.Getenv("HTTP_MAX_IDLE_CONNS_PER_HOST"))
+		if err == nil {
+			maxIdleConnsPerHost = idleCon
+		}
+	}
+	tr := &http.Transport{
+		TLSClientConfig:       &tls.Config{InsecureSkipVerify: true},
+		MaxIdleConns:          maxIdleConns,        // 设置最大空闲连接数
+		MaxIdleConnsPerHost:   maxIdleConnsPerHost, // 设置每个主机的最大空闲连接数
+		IdleConnTimeout:       90 * time.Second,    // 设置空闲连接的超时时间
+		TLSHandshakeTimeout:   10 * time.Second,    // 设置 TLS 握手超时时间
+		ExpectContinueTimeout: 1 * time.Second,     // 设置 100-continue 状态码超时时间
+	}
+	client := &http.Client{Transport: tr}
+	return client
+}
+
 func (c *Component) Start(ctx context.Context, cfg *configs.Config) error {
 	c.url = cfg.APIConfig.ElasticSearchURL
 	c.username = cfg.APIConfig.ElasticSearchUsername
 	c.password = cfg.APIConfig.ElasticSearchPassword
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{Transport: tr}
-	c.client = client
+	c.client = createHTTPClient()
 	return nil
 }
 
@@ -36,11 +63,7 @@ func (c *Component) SingleStart(url, username, password string) {
 	c.url = url
 	c.username = username
 	c.password = password
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{Transport: tr}
-	c.client = client
+	c.client = createHTTPClient()
 }
 
 func (c *Component) CloseHandle() {
