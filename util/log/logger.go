d837908deb9d39ecd66b94f3bcab30aea05a1310
yangk <yangk@goodrain.com>
2024-08-21 02:16:42 +0800

    chore: delete unused comment
    Signed-off-by: yangk <yangk@goodrain.com>

--- util/log/logger.go
+++ util/log/logger.go
@@ -20,14 +20,9 @@ package log
 
 import (
 	"encoding/json"
-	"fmt"
-	"net/http"
+	"github.com/sirupsen/logrus"
 	"os"
 	"strconv"
-	"time"
-
-	"github.com/go-chi/chi/middleware"
-	"github.com/sirupsen/logrus"
 )
 
 type CustomJSONFormatter struct{}
@@ -60,64 +55,4 @@ func InitLogrus() {
 	logrus.SetOutput(os.Stdout)
 	logrus.SetReportCaller(true)
 	logrus.SetFormatter(new(CustomJSONFormatter))
-}
-
-// NewStructuredLogger new struct
-func NewStructuredLogger(logger *logrus.Logger) func(next http.Handler) http.Handler {
-	return middleware.RequestLogger(&StructuredLogger{logger})
-}
-
-// StructuredLogger StructuredLogger
-type StructuredLogger struct {
-	Logger *logrus.Logger
-}
-
-// NewLogEntry NewLogEntry
-func (l *StructuredLogger) NewLogEntry(r *http.Request) middleware.LogEntry {
-	entry := &StructuredLoggerEntry{Logger: logrus.NewEntry(l.Logger)}
-	logFields := logrus.Fields{}
-
-	logFields["ts"] = time.Now().UTC().Format(time.RFC1123)
-
-	if reqID := middleware.GetReqID(r.Context()); reqID != "" {
-		logFields["req_id"] = reqID
-	}
-
-	scheme := "http"
-	if r.TLS != nil {
-		scheme = "https"
-	}
-	logFields["http_scheme"] = scheme
-	logFields["http_proto"] = r.Proto
-	logFields["http_method"] = r.Method
-	logFields["remote_addr"] = r.RemoteAddr
-	logFields["user_agent"] = r.UserAgent()
-	logFields["uri"] = fmt.Sprintf("%s://%s%s", scheme, r.Host, r.RequestURI)
-	entry.Logger = entry.Logger.WithFields(logFields)
-	entry.Logger.Debugln("request started")
-
-	return entry
-}
-
-// StructuredLoggerEntry StructuredLoggerEntry
-type StructuredLoggerEntry struct {
-	Logger logrus.FieldLogger
-}
-
-// Write Write
-func (l *StructuredLoggerEntry) Write(status, bytes int, header http.Header, elapsed time.Duration, data interface{}) {
-	l.Logger = l.Logger.WithFields(logrus.Fields{
-		"resp_status": status, "resp_bytes_length": bytes,
-		"resp_elapsed_ms": float64(elapsed.Nanoseconds()) / 1000000.0,
-	})
-
-	l.Logger.Debugln("request complete")
-}
-
-// Panic Panic
-func (l *StructuredLoggerEntry) Panic(v interface{}, stack []byte) {
-	l.Logger = l.Logger.WithFields(logrus.Fields{
-		"stack": string(stack),
-		"panic": fmt.Sprintf("%+v", v),
-	})
 }
