07c2702c9953ade6e8fdf90bec75d77362f88700
张启航 <101104760+ZhangSetSail@users.noreply.github.com>
2024-10-15 11:04:43 +0800

    perf: change minio storage passwd
    Signed-off-by: 张启航 <101104760+ZhangSetSail@users.noreply.github.com>

--- config/configs/storage_config.go
+++ config/configs/storage_config.go
@@ -12,6 +12,6 @@ type StorageConfig struct {
 func AddStorageFlags(fs *pflag.FlagSet, sc *StorageConfig) {
 	fs.StringVar(&sc.StorageType, "storage-type", "s3", "s3 or local")
 	fs.StringVar(&sc.S3Endpoint, "s3-endpoint", "http://minio-service:9000", "s3 endpoint")
-	fs.StringVar(&sc.S3AccessKeyID, "s3-access-key-id", "minioadmin", "s3 accessKeyID")
-	fs.StringVar(&sc.S3SecretAccessKey, "s3-secret-access-key", "minioadmin", "s3 secretAccessKey")
+	fs.StringVar(&sc.S3AccessKeyID, "s3-access-key-id", "admin1234", "s3 accessKeyID")
+	fs.StringVar(&sc.S3SecretAccessKey, "s3-secret-access-key", "admin1234", "s3 secretAccessKey")
 }
