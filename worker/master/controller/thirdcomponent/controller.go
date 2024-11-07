01e973ff42bdb550bfce73f20f67fcfb32e65821
张启航 <101104760+ZhangSetSail@users.noreply.github.com>
2024-11-07 20:22:17 +0800

    fix: create outer port failure (#1992)
    Signed-off-by: 张启航 <101104760+ZhangSetSail@users.noreply.github.com>

--- worker/master/controller/thirdcomponent/controller.go
+++ worker/master/controller/thirdcomponent/controller.go
@@ -206,12 +206,6 @@ func (r *Reconciler) applyEndpointService(ctx context.Context, log *logrus.Entry
 			return
 		}
 	}
-
-	if err := r.applyer.Apply(ctx, ep); err != nil {
-		log.Errorf("apply endpoint for service %s failure %s", svc.Name, err.Error())
-	}
-
-	ep.Name = ep.Name + "-tcp"
 	if err := r.applyer.Apply(ctx, ep); err != nil {
 		log.Errorf("apply endpoint for service %s failure %s", svc.Name, err.Error())
 	}
